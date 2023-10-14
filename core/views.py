87efa31065c5481682287058b9ccde017571076d
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-10-14 12:40:57 +0530

    ADD: index view function for home page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 
+def index(request):
+    return render(request, "home.html")
+
+
 def test_core_app(request):
     return render(request, "test.html")
