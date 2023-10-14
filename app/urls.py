bd228d7f0c9f48bb170f8ab3fda53796f0bff03a
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-10-14 13:04:51 +0530

    UPDATE: changed URLconf wired to "users" app

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("users/", include("users.urls")),
+    path("account/", include("users.urls")),
 ]
