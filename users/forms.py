cf14299c1ad54eb7abf3dd5b5c6651927aa300a7
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-10-18 15:41:47 +0530

    So many stuff in this commit TT

--- users/forms.py
+++ users/forms.py
@@ -8,13 +8,12 @@ from django.contrib.auth.models import User
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "username", "email", "password"]
+        fields = ["first_name", "last_name", "username", "email"]
 
 
 class LoginForm(forms.Form):
-    class Meta:
-        model = User
-        fields = ["email", "password"]
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 # class RegistrationForm(UserCreationForm):
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from core import views
 
 
@@ -8,25 +10,37 @@ def sign_in(request):
     # form to render on login page (for context dictionary)
     form = LoginForm()
 
+    # title to diplay on login page (for context dictionary)
+    title = "Sign In"
+
     if request.method == "POST":
         # # create instance of LoginForm and populate it using data from POST request
         form = LoginForm(request.POST)
-        # username = request.POST["username"]
-        # password = request.POST["password"]
-        username = form.cleaned_data["username"]
-        password = form.cleaned_data["password"]
+
         if form.is_valid():
-            user = authenticate(username=username, password=password)
+            # get submitted username and password
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            # authenticate user against users in database
+            user = authenticate(request, username=username, password=password)
+
             if user is not None:
                 login(request, user)
-                return render(request, "home.html", {"username": user.get_username()})
+                return redirect("/home/", {"username": username})
+
+            else:
+                messages.error(request, f"Could not find user {username} in system!")
 
     return render(request, "auth/sign-in.html", {"form": form})
 
 
 def sign_up(request):
-    # form to render on login page (for context dictionary)
+    # form to render on sign up page (for context dictionary)
     form = RegistrationForm()
+
+    # title to diplay on sign up page (for context dictionary)
+    title = "Create Account"
 
     # if user has pressed "Submit" on sign-up form:
     if request.method == "POST":
@@ -34,8 +48,11 @@ def sign_up(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # return render(request, "home.html", {"username": request.username})
+
+            # automatically log in the new user
             login(request, user)
+
+            # redirect to the home page, with the new user in the context
             return redirect("/home/", {"username": request.user.username})
 
     return render(request, "auth/sign-up.html", {"form": form})
@@ -47,4 +64,8 @@ def sign_out(request):
 
 
 def delete_account(request):
-    pass
+    if request.user.is_authenticated:
+        user = request.user
+        user.delete()
+        logout(request)
+        return redirect("/")
